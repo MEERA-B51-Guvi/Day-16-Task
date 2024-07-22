2030eb264b2693c1aac3d2fa3a36c04025236d7d
MEERA-B51-Guvi <meerajayapal19@gmail.com>
2024-07-22 14:10:30 +0530

    CallBack

---
+++ script.js
@@ -0,0 +1,56 @@
+
+setTimeout(()=>{
+    document.getElementById("timecount").innerHTML=10;
+    setTimeout(()=>{
+        document.getElementById("timecount").innerHTML=9;
+        setTimeout(()=>{
+            document.getElementById("timecount").innerHTML=8;
+            setTimeout(()=>{
+                document.getElementById("timecount").innerHTML=7
+                setTimeout(()=>{
+                    document.getElementById("timecount").innerHTML=6;
+                    setTimeout(()=>{
+                        document.getElementById("timecount").innerHTML=5;
+                        setTimeout(()=>{
+                            document.getElementById("timecount").innerHTML=4;
+                            setTimeout(()=>{
+                                document.getElementById("timecount").innerHTML=3;
+                                setTimeout(()=>{
+                                    document.getElementById("timecount").innerHTML=2;
+                                    setTimeout(()=>{
+                                        document.getElementById("timecount").innerHTML=1;
+                                       // setTimeout(()=>{
+                                          
+
+                                            setTimeout(()=>{
+                                                //         var x=document.getElementById("imgg").src;
+                                                //               // document.getElementById("timecount").src;
+                                                //               document.getElementById("timecount").innerHTML=x;
+                                                //document.getElementById("timecount").innerHTML="🎉Happy Indepenceday.... 🇮🇳....";
+                                                document.querySelector(".count").innerHTML=" ";
+                                              //  document.getElementById("timecount").style.display="none";
+                                              const x=  document.createElement("img");
+                                                   x.setAttribute("id", "myimg");
+                                                   x.setAttribute("src", "Independance_day_img.jpg");
+                                                //    const x=document.getElementById("myimg").src;
+                                                //  //  const setimg=
+                                                  // document.getElementById("imgg").src;
+                                                   //setimg.appendChild(xx);
+                                                 //  const x=document.getElementById("imgg");
+                                                   const xx=document.querySelector(".count");
+                                                  // xx.style.display="noun";
+                                                   xx.appendChild(x);
+                                                   
+                                                  //  },1000);
+                                        },1000);
+                                     
+                                    },1000);
+                                },1000);
+                            },1000);
+                        },1000);
+                    },1000);
+                },1000);
+            },1000);
+        },1000);
+    },1000);
+},1000);
